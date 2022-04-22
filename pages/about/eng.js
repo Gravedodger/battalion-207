c460132c1feec68c5fbe65e97dc26cf18a4b7b66
Sviatoslav Malyshevskyi <s.malyshevskyi.08@gmail.com>
2022-04-22 14:55:31 +0300

    added english version of the about page

--- pages/about/eng.js
+++ pages/about/eng.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from "../../styles/Styles.module.css";
-import Head from "next/head";
-import { ReturnButtonEng } from "../../components/ReturnButton";
-import Image from "next/image";
 import logo from "../../assets/images/logos/logo-112.png";
+import Head from "next/head";
+import Image from "next/image";
+import { ReturnButtonEng } from "../../components/ReturnButton";
 
 const AboutPageEng = () => {
     return (
-        <div className="container">
+        <div>
             <Head>
                 <meta lang="en"/>
                 <meta name="description" content="Battalion-207 information about the battalion" />
@@ -16,23 +16,44 @@ const AboutPageEng = () => {
             </Head>
 
             <main>
-                <section className="section">
-                    <div className="title">
-                        <ReturnButtonEng />
-                        <h2>About Us</h2>
-                        <div className="underline" />
+                <div className="title">
+                    <ReturnButtonEng />
+                    <h2>About Us</h2>
+                    <div className="underline" />
+                </div>
+
+                <article className={styles.aboutArticle}>
+                    <div className={styles.articleLogoContainer}>
+                        <Image src={logo} alt="battalion logo" />
                     </div>
 
-                    <article className={styles.aboutArticle}>
-                        <div className={styles.articleLogoContainer}>
-                            <Image src={logo} alt="battalion logo" />
-                        </div>
+                    <div className={styles.articleTxtContainer}>
+                        <p>
+                            The 207th Battalion of the 112th Territorial Defense Brigade of Kyiv is a separate unit
+                            within the Armed Forces of Ukraine, subordinated to the Operational Command "North"
+                            (unit A7376).
+                        </p>
 
-                        <div className={styles.articleTxtContainer}>
-                          <p>ENGLISH TEXT VERSION</p>
-                        </div>
-                    </article>
-                </section>
+                        <p>
+                            The Battalion was formed in response to the full-scale invasion of Ukraine by military
+                            forces of the Russian Federation. Both servicemen with combat experience and patriots of
+                            Ukraine without such experience joined its ranks. The atmosphere of a true comradeship,
+                            constant training and the availability of immediate practice ensured every serviceman of
+                            the Battalion is now an experienced soldier standing at the guard of Ukraine and the
+                            rest of Europe.
+                        </p>
+
+                        <p>
+                            Skilful command, combined with bravery of every soldier, helped the Battalion to play an
+                            important role in defense of the Capital of Ukraine, the city of Kyiv.
+                        </p>
+
+                        <p>
+                            Today, individual companies of the Battalion are engaged in combat missions around the Kyiv
+                            region and beyond.
+                        </p>
+                    </div>
+                </article>
             </main>
         </div>
     );
