5bbd90d23818700db3daa4615933b0a5c7076425
Sviatoslav Malyshevskyi <s.malyshevskyi.08@gmail.com>
2022-04-24 07:11:40 +0300

    minor improvement

--- pages/index.js
+++ pages/index.js
@@ -21,49 +21,43 @@ export default function Home() {
               <link rel="icon" href="/favicon.ico" />
           </Head>
 
-          <Box minHeight="100vh" display="flex" flexDir="column">
-              <Container maxW="x1" py={3} display="flex" justifyContent="center" alignItems="center">
-                  <Box w={50} h={50}>
-                      <IconButton position="absolute" left="70%" top="3%" m={3}
-                                  variant="ghost" aria-label="Toggle theme"
-                                  fontSize={40} color="var(--clr-gold)"
-                      onClick={toggleColorMode} icon={themeIcon} />
+          <Container maxW="x1" minHeight="100vh" py={3} display="flex" flexDir="column" justifyContent="center" alignItems="center">
+              <Box w={50} h={50}>
+                  <IconButton position="absolute" left="70%" top="3%" m={3} variant="ghost" aria-label="Toggle theme"
+                              fontSize={40} color="var(--clr-gold)" onClick={toggleColorMode} icon={themeIcon} />
+              </Box>
+
+              <Flex className={styles.starterWrap} flexWrap='wrap' spacing={20} gap={10} mt="5%"
+                    justifyContent='center' alignItems='center' flexDirection='column'>
+                  <Box className={styles.starterLogoContainer}>
+                      <Image src='/logo.png' alt="main logo" objectFit='cover' boxSize="90%" />
                   </Box>
 
-                  <Flex className={styles.starterWrap} flexWrap='wrap' spacing={20} gap={10} mt="5%"
-                        justifyContent='center' alignItems='center' flexDirection='column'>
-                      <Box className={styles.starterLogoContainer}>
-                          <Image src='/logo.png' alt="main logo" objectFit='cover' boxSize="90%" />
+                  <Flex className={styles.starterFlagWrap} justifyContent='center' alignItems='center'>
+                      <Box className={styles.starterFlagContainer}>
+                          <Text fontSize="md" textAlign="center" mb={2}>
+                              Українською
+                          </Text>
+                          <Box className={styles.starterUkrFlag}>
+                              <Link href='homepage/ukr'>
+                                  <Image src='/ukraine-flag.png' alt="flag of Ukraine" objectFit='cover' boxSize="90%" />
+                              </Link>
+                          </Box>
                       </Box>
 
-                      <Flex className={styles.starterFlagWrap} justifyContent='center' alignItems='center'>
-                          <Box className={styles.starterFlagContainer}>
-                              <Text fontSize="md" textAlign="center" mb={2}>
-                                  Українською
-                              </Text>
-                              <Box className={styles.starterUkrFlag}>
-                                  <Link href='homepage/ukr'>
-                                      <Image src='/ukraine-flag.png' alt="flag of Ukraine"
-                                             objectFit='cover' boxSize="90%"  />
-                                  </Link>
-                              </Box>
+                      <Box className={styles.starterFlagContainer}>
+                          <Text fontSize="md" textAlign="center" mb={2}>
+                              In English
+                          </Text>
+                          <Box className={styles.starterEngFlag}>
+                              <Link href="homepage/eng">
+                                  <Image src='/UK-US_flag.png' alt="flag of US/UK" objectFit='cover' boxSize="90%" />
+                              </Link>
                           </Box>
-
-                          <Box className={styles.starterFlagContainer}>
-                              <Text fontSize="md" textAlign="center" mb={2}>
-                                  In English
-                              </Text>
-                              <Box className={styles.starterEngFlag}>
-                                  <Link href="homepage/eng">
-                                      <Image src='/UK-US_flag.png' alt="flag of US/UK"
-                                             objectFit='cover' boxSize="90%"  />
-                                  </Link>
-                              </Box>
-                          </Box>
-                      </Flex>
+                      </Box>
                   </Flex>
-              </Container>
-          </Box>
+              </Flex>
+          </Container>
       </>
   );
 };
