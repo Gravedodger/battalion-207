2dd9f32fb52a12219401d97f9e4331e502d7da2f
Sviatoslav Malyshevskyi <s.malyshevskyi.08@gmail.com>
2022-04-24 05:01:34 +0300

    added document file

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-      <Component {...pageProps} />
+      <ChakraProvider>
+          <Component {...pageProps} />
+      </ChakraProvider>
       )
 }
 
---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ColorModeScript } from '@chakra-ui/react';
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
+
+class Document extends NextDocument {
+    static getInitialProps(ctx) {
+        return NextDocument.getInitialProps(ctx);
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head />
+                <body>
+                    <ColorModeScript initialColorMode="light" />
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default Document;
