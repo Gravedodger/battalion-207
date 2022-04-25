3306a5c359d23584e4953aee61dc9e6f1d88941c
Sviatoslav Malyshevskyi <s.malyshevskyi.08@gmail.com>
2022-04-25 04:36:38 +0300

    added Chakra to about pages

--- pages/about/eng.js
+++ pages/about/eng.js
@@ -1,58 +1,55 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from "../../styles/Styles.module.css";
-import logo from "../../public/logo.png";
 import Head from "next/head";
-import Image from "next/image";
 import Header from "../../components/Header";
+import { Container, Box, Image, Text } from '@chakra-ui/react';
 
 const AboutPageEng = () => {
-    return (
-        <div>
-            <Head>
-                <meta lang="en"/>
-                <meta name="description" content="Battalion-207 information about the battalion" />
-                <link rel="icon" href="/favicon.ico" />
-                <title>Battalion-207 || About</title>
-            </Head>
+    return <>
+        <Head>
+            <meta lang="en"/>
+            <meta name="description" content="Battalion-207 information about the battalion" />
+            <link rel="icon" href="/favicon.ico" />
+            <title>Battalion-207 || About</title>
+        </Head>
 
-            <main>
-               <Header pageTitle="About Us" />
+        <main>
+            <Header pageTitle="About Us" />
 
-                <article className={styles.aboutArticle}>
-                    <div className={styles.articleLogoContainer}>
-                        <Image src={logo} alt="battalion logo" />
-                    </div>
+            <Box className={styles.aboutArticle}>
+                <div className={styles.articleLogoContainer}>
+                    <Image objectFit='fill' src='/logo.png' alt="battalion logo" />
+                </div>
 
-                    <div className={styles.articleTxtContainer}>
-                        <p>
-                            The 207th Battalion of the 112th Territorial Defense Brigade of Kyiv is a separate unit
-                            within the Armed Forces of Ukraine, subordinated to the Operational Command "North"
-                            (unit A7376).
-                        </p>
+                <Container className={styles.articleTxtContainer} fontSize='xl' textAlign='justify'>
+                    <Text>
+                        The 207th Battalion of the 112th Territorial Defense Brigade of Kyiv is a separate unit
+                        within the Armed Forces of Ukraine, subordinated to the Operational Command "North"
+                        (unit A7376).
+                    </Text>
 
-                        <p>
-                            The Battalion was formed in response to the full-scale invasion of Ukraine by military
-                            forces of the Russian Federation. Both servicemen with combat experience and patriots of
-                            Ukraine without such experience joined its ranks. The atmosphere of a true comradeship,
-                            constant training and the availability of immediate practice ensured every serviceman of
-                            the Battalion is now an experienced soldier standing at the guard of Ukraine and the
-                            rest of Europe.
-                        </p>
+                    <Text>
+                        The Battalion was formed in response to the full-scale invasion of Ukraine by military
+                        forces of the Russian Federation. Both servicemen with combat experience and patriots of
+                        Ukraine without such experience joined its ranks. The atmosphere of a true comradeship,
+                        constant training and the availability of immediate practice ensured every serviceman of
+                        the Battalion is now an experienced soldier standing at the guard of Ukraine and the
+                        rest of Europe.
+                    </Text>
 
-                        <p>
-                            Skilful command, combined with bravery of every soldier, helped the Battalion to play an
-                            important role in defense of the Capital of Ukraine, the city of Kyiv.
-                        </p>
+                    <Text>
+                        Skilful command, combined with bravery of every soldier, helped the Battalion to play an
+                        important role in defense of the Capital of Ukraine, the city of Kyiv.
+                    </Text>
 
-                        <p>
-                            Today, individual companies of the Battalion are engaged in combat missions around the Kyiv
-                            region and beyond.
-                        </p>
-                    </div>
-                </article>
-            </main>
-        </div>
-    );
-};
+                    <Text>
+                        Today, individual companies of the Battalion are engaged in combat missions around the Kyiv
+                        region and beyond.
+                    </Text>
+                </Container>
+            </Box>
+        </main>
+    </>
+}
 
 export default AboutPageEng;
--- pages/about/ukr.js
+++ pages/about/ukr.js
@@ -1,57 +1,53 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from "../../styles/Styles.module.css";
 import Head from "next/head";
-import logo from '/public/logo.png';
-import Image from "next/image";
 import Header from "../../components/Header";
+import { Container, Box, Image, Text } from '@chakra-ui/react';
 
-const AboutPageUkr = ({pageTitle}) => {
-    return (
-        <div>
-            <Head>
-                <meta lang="uk"/>
-                <meta name="description" content="Battalion-207 інформація про батальйон" />
-                <link rel="icon" href="/favicon.ico" />
-                <title>Battalion-207 || Про Батальйон</title>
-            </Head>
+const AboutPageUkr = ({ pageTitle }) => {
+    return <>
+        <Head>
+            <meta lang="uk"/>
+            <meta name="description" content="Battalion-207 інформація про батальйон" />
+            <link rel="icon" href="/favicon.ico" />
+            <title>Battalion-207 || Про Батальйон</title>
+        </Head>
 
-            <main>
-                <Header pageTitle='Про Батальйон' />
+        <main>
+            <Header pageTitle='Про Батальйон' />
 
-                <article className={styles.aboutArticle}>
-                    <div className={styles.articleLogoContainer}>
-                        <Image src={logo} alt="battalion logo" />
-                    </div>
+            <Box className={styles.aboutArticle}>
+                <div className={styles.articleLogoContainer}>
+                    <Image objectFit='fill' src='/logo.png' alt="battalion logo" />
+                </div>
 
-                    <div className={styles.articleTxtContainer}>
-                        <p>
-                            207-й батальйон 112-ої бригади Територіальної оборони м.Києва є окремим підрозділом
-                            у складі Збройних Сил України, підпорядкований Оперативному командуванню "Північ"
-                            (в/ч А7376).
-                        </p>
+                <Container className={styles.articleTxtContainer} fontSize='xl' textAlign='justify'>
+                    <Text>
+                        207-й батальйон 112-ої бригади Територіальної оборони м.Києва є окремим підрозділом у складі Збройних Сил України, підпорядкований Оперативному командуванню "Північ"
+                        (в/ч А7376).
+                    </Text>
 
-                        <p>
-                            Батальйон було створено у відповідь на повномасштабне вторгнення окупаційних військ
-                            російської федерації на територію України. До його лав стали як військові з бойовим
-                            досвідом, так і патріоти України без такого досвіду. Атмосфера справжньго братерства,
-                            постійні тренування та наявність безпосередньої практики дозволили кожному військовослужбовцю
-                            батальйону стати досвідченим воїном на варті України та решти Європи.
-                        </p>
+                    <Text>
+                        Батальйон було створено у відповідь на повномасштабне вторгнення окупаційних військ
+                        російської федерації на територію України. До його лав стали як військові з бойовим
+                        досвідом, так і патріоти України без такого досвіду. Атмосфера справжньго братерства,
+                        постійні тренування та наявність безпосередньої практики дозволили кожному військовослужбовцю
+                        батальйону стати досвідченим воїном на варті України та решти Європи.
+                    </Text>
 
-                        <p>
-                            Вміле командування та відчайдушність бійців допомогли батальйону зіграти важливу роль у
-                            обороні столиці України, міста Києва.
-                        </p>
+                    <Text>
+                        Вміле командування та відчайдушність бійців допомогли батальйону зіграти важливу роль у
+                        обороні столиці України, міста Києва.
+                    </Text>
 
-                        <p>
-                            На сьогодні окремі роти батальйону виконують бойові завдання в Київській області та за її
-                            межами.
-                        </p>
-                    </div>
-                </article>
-            </main>
-        </div>
-    );
+                    <Text>
+                        На сьогодні окремі роти батальйону виконують бойові завдання в Київській області та за її
+                        межами.
+                    </Text>
+                </Container>
+            </Box>
+        </main>
+    </>
 };
 
 export default AboutPageUkr;
