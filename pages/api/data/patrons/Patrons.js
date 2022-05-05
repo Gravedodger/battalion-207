c48771291cf335adda5cfe1dbc59faff6a1d43b3
Sviatoslav Malyshevskyi <s.malyshevskyi.08@gmail.com>
2022-05-05 04:01:41 +0300

    small fix

--- pages/api/data/patrons/Patrons.js
+++ pages/api/data/patrons/Patrons.js
@@ -1,21 +1,130 @@
 const Patrons = [
+    // 06 APRIL 2022
     {
         id: 1,
-        name: 'Sviatoslav Malyshevskyi',
-        currency: 'USD',
-        amount: 300,
+        date: '06.04.2022',
+        name: 'Yuliana N.',
+        amount: 1000,
+        currency: 'UAH',
     },
     {
         id: 2,
-        name: 'Ivan I.',
-        currency: 'EUR',
-        amount: 100,
+        date: '06.04.2022',
+        name: 'Natalia Sh.',
+        amount: 1000,
+        currency: 'UAH',
     },
     {
         id: 3,
-        name: 'Ruslan  M,',
+        date: '06.04.2022',
+        name: 'unknown person',
+        amount: 1000,
         currency: 'UAH',
-        amount: 2300,
+    },
+
+    // 07 APRIL 2022
+    {
+        id: 4,
+        date: '07.04.2022',
+        name: 'Kamil I.',
+        amount: 100,
+        currency: 'USD',
+    },
+
+    // 08 APRIL 2022
+    {
+        id: 5,
+        date: '08.04.2022',
+        name: 'Hanna K.',
+        amount: 2000,
+        currency: 'UAH',
+    },
+
+    // 11 APRIL 2022
+    {
+        id: 6,
+        date: '11.04.2022',
+        name: 'unknown person',
+        amount: 50,
+        currency: 'EUR',
+    },
+
+    // 12 APRIL 2022
+    {
+        id: 7,
+        date: '12.04.2022',
+        name: 'Kadri V.',
+        amount: 150,
+        currency: 'EUR',
+    },
+    {
+        id: 8,
+        date: '12.04.2022',
+        name: 'Ivan D.',
+        amount: 1000,
+        currency: 'UAH',
+    },
+    {
+        id: 9,
+        date: '12.04.2022',
+        name: 'unknown person',
+        amount: 3000,
+        currency: 'UAH',
+    },
+    {
+        id: 10,
+        date: '12.04.2022',
+        name: 'Aleksandra B.',
+        amount: 350,
+        currency: 'UAH',
+    },
+    {
+        id: 11,
+        date: '12.04.2022',
+        name: 'Tetiana B.',
+        amount: 1000,
+        currency: 'UAH',
+    },
+    {
+        id: 12,
+        date: '12.04.2022',
+        name: 'Hanna K.',
+        amount: 300,
+        currency: 'USD',
+    },
+
+    // 13 APRIL 2022
+    {
+        id: 13,
+        date: '13.04.2022',
+        name: 'Pavlo M.',
+        amount: 555,
+        currency: 'UAH',
+    },
+    {
+        id: 14,
+        date: '13.04.2022',
+        name: 'Margus H.',
+        amount: 200,
+        currency: 'USD',
+    },
+
+    // 18 APRIL 2022
+    {
+        id: 15,
+        date: '18.04.2022',
+        name: 'Niklas L.',
+        amount: 260,
+        currency: 'USD',
+    },
+
+    // 19 APRIL 2022
+    {
+        id: 16,
+        date: '19.04.2022',
+        name: 'Simon D.',
+        amount: 103,
+        currency: 'USD',
     }
 ]
 
