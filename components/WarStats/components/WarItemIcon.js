8feb491901993efa70f76f903d2cc85de07feb15
Sviatoslav Malyshevskyi <s.malyshevskyi.08@gmail.com>
2022-05-15 00:47:45 +0300

    updated build

--- components/WarStats/components/WarItemIcon.js
+++ components/WarStats/components/WarItemIcon.js
@@ -4,6 +4,7 @@ import { Box, Image } from '@chakra-ui/react';
 const WarItemIcon = ({ icon }) => {
     return <Image src={icon}
                   width={{ lg: '60px', md: '50px', sm: '40px', base: '30px' }}
+                  height='auto'
                   filter='invert(72%) sepia(37%) saturate(456%) hue-rotate(357deg) brightness(82%) contrast(95%)'
     />
 }
