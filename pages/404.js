f7a7055c07b57fc8d1c141a1a0402d23a215e7ea
Sviatoslav Malyshevskyi <s.malyshevskyi.08@gmail.com>
2022-05-24 23:23:11 +0300

    added toggle theme to 404 error page

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
-import { Center, Box, Text, Image } from '@chakra-ui/react';
+import { Center, Box, Text, Image, IconButton, Container } from '@chakra-ui/react';
 
 export default function Custom404() {
     return <>
         <Center flexDir='column' mt='10rem'>
+            <Box w='100%'>
+                <IconButton
+                    position='relative'
+                    left='80%'
+                    mt='2rem'
+                    fontSize='40px'
+                    variant="ghost"
+                    aria-label="Toggle theme"
+                    cursor='pointer'
+                    icon={themeIcon}
+                    onClick={toggleColorMode}
+                    color='#c59d5f'
+                />
+            </Box>
+
             <Box width={{ lg: '17rem', md: '13rem', sm: '10rem', base: '10rem' }} ml='1.7rem' mb='5rem'>
                 <Image src='/logo.WebP' lazy='true' alt="main logo" objectFit='contain' boxSize="90%" layout='fill'/>
             </Box>
